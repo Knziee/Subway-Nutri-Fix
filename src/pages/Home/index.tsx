9a81da4209caf0fb6e3f65f6e847aa3c47f5880e
Knziee <16rhuan16@gmail.com>
2022-04-14 00:07:05 -0300

    feat: inicio da nova ui

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,25 +1,14 @@
-import { InfosContainer } from '../../components/InfosContainer';
+
 import {Buttons} from '../../components/Buttons';
-import  {BackgroundContainer, LeftWrapper, RightWrapper } from './styles';
-import { BigCircle } from '../../components/BigCircle'
-import {MediumCircle} from '../../components/MediumCircle'
-import {SmallCircle} from '../../components/SmallCircle'
+import  {BackgroundContainer } from './styles';
 
 export const Home: React.FC = () => {
     return (
         <BackgroundContainer>
-            <LeftWrapper>
-        <InfosContainer>
 
-        </InfosContainer>
 
         <Buttons />
-        </LeftWrapper>
-        <RightWrapper>
-            <BigCircle/>
-            <MediumCircle/>
-            <SmallCircle/>
-        </RightWrapper>
+
         </BackgroundContainer>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -11,21 +11,3 @@ justify-content: flex-start;
 align-items: space-around;
  `;
 
-export const LeftWrapper = styled.div`
-width: 24.427vw;
-height:1080px;
-display:flex;
-flex-wrap: wrap;
-
-    `;
-
-export const RightWrapper = styled.div`
-width: 24.427vw;
-height:1080px;
-display:flex;
-justify-content: center;
-flex-wrap: wrap;
-flex-direction: column;
-align-items: center;
-
-    `;
