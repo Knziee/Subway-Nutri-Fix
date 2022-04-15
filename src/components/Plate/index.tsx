7e038470b91d855746747d1c137e72a886b26ad5
Knziee <16rhuan16@gmail.com>
2022-04-15 05:32:27 -0300

    adicionado grafico, contador de kcal e sodio

--- src/components/Plate/index.tsx
+++ src/components/Plate/index.tsx
@@ -1,9 +1,23 @@
 import { useState } from 'react'
-import { PlateContainer,BreadPositionTop,PlatePosition, GraphInfo, BreadPositionBottom, InfoContainer } from './styles'
+import { 
+  PlateContainer ,
+  BreadPositionTop,
+   PlatePosition,
+    GraphInfo, 
+    BreadPositionBottom, 
+    InfoContainer, 
+    PlateText,
+    KcalCircle,
+    SizeDisplay,
+    SodiumBar,
+    SodiumBarText
+   } from './styles'
 import GraphInfoImg from '../../assets/images/GraficoLegenda.svg'
 import PlateImg from '../../assets/images/Prato1pixelzada.svg'
+import ArrowSodium from '../../assets/images/Seta.svg'
 import PlateImg2 from '../../assets/images/prato2pixelzada.svg'
 import {Buttons} from '../Buttons';
+import { PieChart, Pie, Cell } from "recharts";
 
 //paes
 import noveGraos from '../../assets/images/paes/9graosCima.svg'
@@ -17,70 +31,200 @@ import parmesaEOreganoInferior from '../../assets/images/paes/parmesaoComOregano
 import aveiaEMelInferior from '../../assets/images/paes/aveiaEmelBaixo.svg'
 import italianoBrancoInferior from '../../assets/images/paes/brancItalianoBaixo.svg'
 
-interface SandwichProps {
-
-}
-
 export const Plate: React.FC = () => {
-const [count, setCount] = useState(0)
+const [countSize, setCountSize] = useState(0)
+const [countBread, setCountBread] = useState(0)
 
-  const paes = [
+let size: any
+size = [
+  {
+    name: '15cm',
+    category: 'Size',
+    carb: 1,
+    fat:1,
+    protein:1,
+    kcal:1,
+    sodium: 1,
+    },  
     {
-    name: '',
+      name: '30cm',
+      category: 'Size',
+      carb:2,
+      fat:2,
+      protein:2,
+      kcal:2,
+      sodium: 2,
+      },
+]
+
+let bread: any
+
+bread = [
+    {
+    name: '9 Grãos',
+    category: 'Pão',
     img: noveGraos,
     img2: noveGraosInferior,
-    kcal:'',
-    fat:'',
-    protein:'',
+    carb:40,
+    fat:0.3,
+    protein:8.5,
+    kcal:208,
+    sodium:226.
     },  
     {
-    name: '',
+    name: '3 Queijos',
+    category: 'Pão',
     img: tresQueijos,
     img2: tresQueijosInferior,
+    carb:36,
+    fat:1.3,
+    protein:12,
+    kcal:219,
+    sodium:271.
     },
     {
-    name: '',
+    name: 'Parmesão e Oregano',
+    category: 'Pão',
     img: parmesaEOregano,
     img2: parmesaEOreganoInferior,
+    carb:37,
+    fat:0.4,
+    protein:11,
+    kcal:205,
+    sodium:383.
     },  
     {
-    name: '',
-    img: aveiaEMel,
-    img2: aveiaEMelInferior,
-    },
-    {
-    name: '',
+    name: 'Italiano Branco',
+    category: 'Pão',
     img: italianoBranco,
     img2: italianoBrancoInferior,
+    carb:36,
+    fat:0.3,
+    protein:11,
+    kcal:198,
+    sodium:214.
     },       
-  ]
+        // {
+    // name: '9 Grãos Aveia e Mel',
+    // category: 'Pão',
+    // img: aveiaEMel,
+    // img2: aveiaEMelInferior,
+    // carb:999,
+    // fat:999,
+    // protein:999,
+    // kcal:999,
+    // },
+  ]  
+  const carbCalc = size[countSize].carb * bread[countBread].carb 
+  const fatCalc = size[countSize].fat *  bread[countBread].fat 
+  const proteinCalc = size[countSize].protein *  bread[countBread].protein 
+  const kcalCalc = size[countSize].kcal *  bread[countBread].kcal 
+  const sodiumCalc = size[countSize].kcal *  bread[countBread].sodium 
 
+  const foodData = [
+    { name: "Carboidrato", value: carbCalc },
+    { name: "Gordura", value: fatCalc },
+    { name: "Proteína", value: proteinCalc },
+    
+  ];
+  const colors = ["#0088FE", "#00C49F", "#FFBB28"];
 
+  const radian = Math.PI / 180;
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index
+}: any) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * radian);
+  const y = cy + radius * Math.sin(-midAngle * radian);
+
+  return (
+    <text 
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+};
 
     return (<div>
 <InfoContainer>
 <GraphInfo>
-<img src={GraphInfoImg} alt=''/>
+{/* <img src={GraphInfoImg} alt=''/> */}
 </GraphInfo>
+<PieChart width={400} height={400} style={{ marginTop: '-3vw', transform:'scale(1.0)'
+}}>
+        <Pie
+          data={foodData}
+          cx={200}
+          cy={200}
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {foodData.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
+        </Pie>
+      </PieChart>
+      <KcalCircle>
+        {/* kcal */}
+<p>Calorias</p>
+<span>{kcalCalc}</span>
+</KcalCircle>
+
 </InfoContainer>
+
+{/* sodium */}
+<SodiumBar>
+<img src ={ArrowSodium} alt='' />
+  <SodiumBarText>
+
+  Sódio: {sodiumCalc}
+  </SodiumBarText>
+</SodiumBar>
 
 <PlateContainer>
 <PlatePosition>
+
+  {/* size */}
+<SizeDisplay>
+  {size[countSize].name}
+</SizeDisplay>
 <img src={PlateImg2} alt ='' />
+<PlateText>
+<p>{bread[countBread].category}</p>
+{bread[countBread].name}
+</PlateText>
 </PlatePosition>
 
-{/* paes */}
+{/* breads */}
 <BreadPositionBottom>
-<img src={paes[count].img2} alt ='' />
+<img src={bread[countBread].img2} alt ='' />
 </BreadPositionBottom>
 <BreadPositionTop>
-  <img src={paes[count].img} alt ='' />
+  <img src={bread[countBread].img} alt ='' />
   </BreadPositionTop>
 
-<p>ta na array {count} </p>
+<p>ta na array {countBread} </p>
 
-<button onClick={() => count === paes.length-1 ?  setCount(0) : setCount(count + 1)}>
-    clica em eu funciona pfvr
+<button onClick={() => countBread === bread.length-1 ?  setCountBread(0) : setCountBread(countBread + 1)}>
+    BREAD
+</button>
+
+<button onClick={() => countSize === size.length-1 ?  setCountSize(0) : setCountSize(countSize + 1)}>
+    SIZE
 </button>
 
 </PlateContainer>
--- src/components/Plate/styles.ts
+++ src/components/Plate/styles.ts
@@ -8,31 +8,56 @@ display:flex;
 align-items: center;
 justify-content:center;
 flex-direction:column;
+margin-top: -4vw;
+
 `;
 export const InfoContainer = styled.div `
-background-color: red;
-opacity: 20%;
+float: right;
 
 display:flex;
 align-items: center;
-justify-content:space-between;
-flex-direction:row;
+justify-content:center;
+flex-direction:column;
+margin-right: -3vw;
+margin-top: -4vw;
 `;
 
 
 export const BreadPositionTop = styled.div `
 position: absolute;
-margin-bottom: 6.3vw;
+margin-bottom: 10.3vw;
 margin-left:1vw;
 `;
 export const BreadPositionBottom = styled.div `
 position: absolute;
-margin-bottom: 7vw;
+margin-bottom: 11vw;
 margin-left:1vw;
 `;
 
 export const PlatePosition = styled.div `
+img {
+    width: 600px; 
+    height: 150px; 
+    object-fit: cover
+}
+`;
+export const SizeDisplay = styled.div `
+margin-left: 90px;
+font-size: 30px;
+margin-top: -20px;
+color: #FFCD3F;
+`;
+export const PlateText = styled.div `
+font-size: 50px;
+text-align: center;
+color: #ffff;
 
+p {
+    font-size: 30px;
+    text-align: center;
+
+
+}
 `;
 
 export const GraphInfo = styled.div `
@@ -42,4 +67,48 @@ img {
     margin-top: 4vw;
     margin-bottom: 1vw;
 }
+`;
+
+export const KcalCircle = styled.div `
+display: flex;
+align-items: right ;
+flex-direction: row;
+flex-wrap:wrap;
+
+margin-top: -4vw;
+  height: 100px;
+  width: 100px;
+  background-color: #0F9246;
+  border: solid 10px #FFCD3F;
+  border-radius: 50%;
+
+  p {
+    font-size: 15px;
+      display: flex;
+      align-items: center;
+    margin-left: 14px;
+    margin-top: 15px;
+    color:#ffff;
+    text-transform: uppercase;
+  }
+span {
+  font-size: 30px;
+  margin-top:-15px;
+  margin-left: 28px;
+  color:#FFCD3F
+}
+
+`;
+export const SodiumBar = styled.div `
+margin-top: 130px;
+
+
+`;
+export const SodiumBarText = styled.div `
+
+font-size: 25px;
+color:#ffff;
+margin-left: 30px;
+margin-top: -60px
+
 `;
